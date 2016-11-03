873ac20ba3318dd34b8c94d6d26a8b7b4ba43f2f
Patrick Miller <thepdm@gmail.com>
2016-11-04 02:55:04 +0900

    Now published on GreasyFork

--- wanikani-hints.user.js
+++ wanikani-hints.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani Hints
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.wanikani.com/review/session
